869b117c9742061853f9d3b4203166bb46830ef5
Elias Oliveira <elias_albuquerque@yahoo.com.br>
2024-07-08 17:02:40 -0300

    adicionando dropdown com multi choice

--- UI/src/app/feature/blog-post/add-blogpost/add-blogpost.component.ts
+++ UI/src/app/feature/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
@@ -6,25 +6,29 @@ import { FormsModule } from '@angular/forms';
 import { BlogPostService } from '../services/blog-post.service';
 import { response } from 'express';
 import { MarkdownModule, MarkdownService } from 'ngx-markdown';
+import { CategoryService } from '../../category/services/category.service';
+import { Category } from '../../category/models/category.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-add-blogpost',
   standalone: true,
   imports: [RouterModule,
-            FormsModule,
-            CommonModule,
-            MarkdownModule
+    FormsModule,
+    CommonModule,
+    MarkdownModule
 
   ],
   templateUrl: './add-blogpost.component.html',
   styleUrl: './add-blogpost.component.css'
 })
-export class AddBlogpostComponent {
+export class AddBlogpostComponent implements OnInit {
   model: AddBlogPost;
+  categories$?: Observable<Category[]>
 
   constructor(private blogPostService: BlogPostService,
-    private router: Router
-
+    private router: Router,
+    private categoryService: CategoryService
   ) {
     this.model = {
       title: '',
@@ -37,6 +41,10 @@ export class AddBlogpostComponent {
       publishedDate: new Date(),
       categories: []
     }
+  }
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
+
   }
 
   onFormSubmit(): void {
