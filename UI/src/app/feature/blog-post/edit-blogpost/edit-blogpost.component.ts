710a391ae9f89cdb5908af388f387a6fa242dcc4
Elias Oliveira <elias_albuquerque@yahoo.com.br>
2024-07-08 14:41:51 -0300

    inclusão de imports removidos do Edit =>  imports: [FormsModule, CommonModule, RouterModule],

--- UI/src/app/feature/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ UI/src/app/feature/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-edit-blogpost',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './edit-blogpost.component.html',
   styleUrl: './edit-blogpost.component.css'
 })
